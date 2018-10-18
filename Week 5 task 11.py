ef492785e646f5484fb0306895005b63f129cb2e
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2018-10-18 18:50:19 +0100

    Update Week 5 task 11.py

--- Week 5 task 11.py
+++ Week 5 task 11.py
@@ -7,7 +7,10 @@ while UserInput != password:
         UserInput=input("Try again\n")
         
         AttemptCounter = AttemptCounter+1
+        if AttemptCounter == 5:
+            print("Access denied, please press enter to exit and contact security to reset your password")
+            
 
 if UserInput == password:
         print("Accepted")
-        print("number of attempts = " + str(AttemptCounter))    
+        print("number of attempts = " + str(AttemptCounter))  
