a246a245559a774b7f69c25ca00e5df14c803139
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2018-10-23 15:54:41 +0100

    Update Week 5 task 11.py

--- Week 5 task 11.py
+++ Week 5 task 11.py
@@ -1,14 +1,22 @@
+# The variable password is set to the string "change me"
 password = "changeme"
+#counter is set to one 
 AttemptCounter= 1
 
 UserInput=input("Please type your password\n")
-
+#This loop is repeated until the UserInput is equal to the value of the passowrd variable
 while UserInput != password:
-        UserInput=input("Try again\n")
-        
-        AttemptCounter = AttemptCounter+1
+
         if AttemptCounter == 5:
             print("Access denied, please press enter to exit and contact security to reset your password")
+    #When the input doesn't equal to the value of passoword an output is displayed.
+    #The user can try again as the value of the variable of UserInput is changed.
+        UserInput=input("Try again\n")
+        #+1 is added to the value of the counter
+        AttemptCounter = AttemptCounter+1
+
+        #When there has been 5 tries then the Access is denied.
+       
             
 
 if UserInput == password:
