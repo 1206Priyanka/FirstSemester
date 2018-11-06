578d69ca5d452f8171b4045059f2a6b48262b901
1206Priyanka <43996054+1206Priyanka@users.noreply.github.com>
2018-11-06 19:41:42 +0000

    Update Week 5 task 10.py

--- Week 5 task 10.py
+++ Week 5 task 10.py
@@ -1,12 +1,17 @@
-richter = float(input("What is the magnitude of the eathquake?"))
+#The richter value is set to what the user inputs and converted to a float.
+richter = float(input("What is the magnitude of the eathquake?\n"))
+
+#if richter value is 8 or higher then Most structures fall will be the output.
 if richter >= 8 :
               print("Most structures fall")
 
+#elif function is used to give different outputs for different richter value.
 elif richter >=7:
               print("Many buildings destroyed")
+              
 
 elif richter >= 6:
-              print("Many buildings considerably damaged some colla[se")
+              print("Many buildings considerably damaged some collapse")
 
 elif richter >= 4.5 :
               print("Damage to poorly constructed buildings")
